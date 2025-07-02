643efca74066e26820bd3cc6eae6c7086103b026
DiegoPmz <diego.pmz08@gmail.com>
2025-07-01 23:01:18 -0600

    se creo la sección ProjectCtaSection

--- src/components/ui/GradientButton.tsx
+++ src/components/ui/GradientButton.tsx
@@ -4,12 +4,14 @@ import type { PropsWithChildren } from "react";
 export const GradientButton = ({
   children,
   size,
-}: PropsWithChildren<{ size?: "sm" }>) => {
+  className,
+}: PropsWithChildren<{ size?: "sm"; className?: string }>) => {
   return (
     <button
       className={cn(
         "cursor-pointer w-full group relative bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600 rounded-2xl px-6 py-4 transition-all duration-500 overflow-hidden",
         size && "w-fit px-3 py-2",
+        className,
       )}
     >
       <div className="absolute inset-0 bg-gradient-to-r from-white/10 to-white/5 opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
